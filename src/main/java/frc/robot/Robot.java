3c839a28e4982d58e90c5a4266aae2d551030fa9
neev <nevadex@gmail.com>
2023-11-16 14:11:51 -0500

    condensed and annotated code. will eventually make the actual files to make it make sense

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -1,66 +1,56 @@
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
-
 package frc.robot;
 
-import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.TimedRobot;
-import edu.wpi.first.wpilibj.motorcontrol.MotorController;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.RelativeEncoder;
-import com.revrobotics.SparkMaxAbsoluteEncoder;
-import com.revrobotics.SparkMaxRelativeEncoder;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 /**
- * This sample program shows how to control a motor using a joystick. In the operator control part
- * of the program, the joystick is read and the value is written to the motor.
- *
- * <p>Joystick analog values range from -1 to 1 and motor controller inputs also range from -1 to 1
- * making it easy to work together.
- *
- * <p>In addition, the encoder value of an encoder connected to ports 0 and 1 is consistently sent
- * to the Dashboard.
+This sample program shows how to control a motor using a joystick.
+
+Certain lines of code include comments indicating the purpose.
+Some comments also have indications towards which file this code would reside in an actual robot
+Read through to develop your understanding of how robotics code works.
  */
 public class Robot extends TimedRobot {
-  private static final int kMotorPort = 2;
-  private static final int kJoystickPort = 0;
-  //private static final int kEncoderPortA = 0;
-  //private static final int kEncoderPortB = 1;
+  // The following two constants are usually defined in a Constants.java file
+  private static final int MOTOR_CAN_ID = 2; // Defines the CAN id of the Spark Max motor controller
+  private static final int JOYSTICK_PORT = 0; // Defines the port id of the joystick
 
-  private CANSparkMax m_motor;
-  private Joystick m_joystick;
-  private RelativeEncoder m_encoder;
+  // The following three variables would be defined within a subsystem.
+  private CANSparkMax motor;
+  private Joystick joystick;
+  private RelativeEncoder encoder; // The encoder is directly attached to the Spark Max, no PWM needed
 
   @Override
   public void robotInit() {
-    m_motor = new CANSparkMax(kMotorPort, MotorType.kBrushless);
-    m_encoder = m_motor.getEncoder();
-    m_joystick = new Joystick(kJoystickPort);
-    //m_encoder = new SparkMaxRelativeEncoder(m_motor, Encoder, 1);
-    //m_encoder = new Encoder(kEncoderPortA, kEncoderPortB);
-    // Use SetDistancePerPulse to set the multiplier for GetDistance
-    // This is set up assuming a 6 inch wheel with a 360 CPR encoder.
-    //m_encoder.setDistancePerPulse((Math.PI * 6) / 360.0);
+    // robot container should be defined
+
+    // The following three statements would be located within a subsystem.
+    motor = new CANSparkMax(MOTOR_CAN_ID, MotorType.kBrushless); // Create the Spark Max object in the program and include the CAN id and the type of motor used with the controller
+    encoder = motor.getEncoder(); // Grab the attached encoder
+    joystick = new Joystick(JOYSTICK_PORT); // Create the joystick object
   }
 
-  /*
-   * The RobotPeriodic function is called every control packet no matter the
-   * robot mode.
-   */
   @Override
   public void robotPeriodic() {
-    SmartDashboard.putNumber("Encoder", m_encoder.getVelocity());
-    SmartDashboard.putNumber("JoyPos", m_joystick.getY());
-    System.out.println("command: " + m_joystick.getY() + " drive: " + m_motor.getAppliedOutput() + " encvel:" + m_encoder.getVelocity() );
+    // The following three calls would be located within a subsystem
+    SmartDashboard.putNumber("Encoder", encoder.getVelocity());
+    SmartDashboard.putNumber("JoyPos", joystick.getY());
+    System.out.println("command: " + joystick.getY() + " drive: " + motor.getAppliedOutput() + " encvel:" + encoder.getVelocity() );
+  }
+
+  @Override
+  public void teleopInit() {
+    // The scheduler should start using the teleop command from robot container
   }
 
   @Override
   public void teleopPeriodic() {
-    m_motor.set(m_joystick.getY());
+    // The following call would be located within a subsystem.
+    motor.set(joystick.getY());
   }
 }
