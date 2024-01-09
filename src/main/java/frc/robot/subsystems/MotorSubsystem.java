25214513e217e4925cab5afe6d8d48ea104c9e5d
neev <nevadex@gmail.com>
2024-01-09 00:14:10 -0500

    update to 2024 wpilib and confirm building

--- src/main/java/frc/robot/subsystems/MotorSubsystem.java
+++ src/main/java/frc/robot/subsystems/MotorSubsystem.java
@@ -1,7 +1,7 @@
 package frc.robot.subsystems;
 
 import com.revrobotics.*;
-import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+import com.revrobotics.CANSparkLowLevel.MotorType;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
