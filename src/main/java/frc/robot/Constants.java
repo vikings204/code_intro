2f6e0912fd50e75564b28600555cf119be633228
Tjbots15 <tanmayjena2027@eccrsd.us>
2024-01-11 15:01:59 -0500

    By Tanmay Jena, code for RobotContainer

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -1,6 +1,6 @@
 package frc.robot;
 
 public final class Constants {
-  public static final int MOTOR_CAN_ID = 2; // Defines the CAN id of the Spark Max motor controller
+  public static final int MOTOR_CAN_ID = 12; // Defines the CAN id of the Spark Max motor controller
   public static final int JOYSTICK_PORT = 0; // Defines the port id of the joystick
 }
