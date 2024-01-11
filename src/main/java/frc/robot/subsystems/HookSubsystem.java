f398685f0c6c9b3dbb4030f0d4409c150176d35e
Tjbots15 <tanmayjena2027@eccrsd.us>
2024-01-11 15:50:23 -0500

    Added Raise and Down functions

--- src/main/java/frc/robot/subsystems/HookSubsystem.java
+++ src/main/java/frc/robot/subsystems/HookSubsystem.java
@@ -6,9 +6,19 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 
-public class MotorSubsystem extends SubsystemBase {
+public class HookSubsystem extends SubsystemBase {
     private CANSparkMax motor = new CANSparkMax(Constants.MOTOR_CAN_ID, MotorType.kBrushless); // Create the Spark Max object in the program and include the CAN id and the type of motor used with the controller
     private RelativeEncoder encoder = motor.getEncoder(); // Grab the attached encoder
+
+    public void raiseMotor(double joyY) {
+        System.out.println("Raising Hook");
+        motor.set(joyY);
+    }
+
+    public void downHook(double joyY) {
+        System.out.println("Down Hook");
+        motor.set(joyY);
+    }
 
     public void setMotor(double joyY) {
         SmartDashboard.putNumber("Encoder", encoder.getVelocity());
