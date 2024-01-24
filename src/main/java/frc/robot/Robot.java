03c77dcfd23986d8fae496b7cd6af298feadd30b
Tjbots15 <tanmayjena2027@eccrsd.us>
2024-01-24 16:28:40 -0500

    Added basis for code for AprilTag, will be changed later. Also, made TeleopPeriodic()

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -5,6 +5,19 @@ import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import frc.robot.subsystems.HookSubsystem;
+import edu.wpi.first.apriltag.AprilTagDetection;
+import edu.wpi.first.apriltag.AprilTagDetector;
+import edu.wpi.first.apriltag.AprilTagPoseEstimator;
+import edu.wpi.first.cameraserver.CameraServer;
+import edu.wpi.first.cscore.CvSink;
+import edu.wpi.first.math.geometry.Transform3d;
+
+import org.opencv.core.Mat;
+import org.opencv.imgproc.Imgproc;
+import org.photovision.vision.opencv.CVMat;
+import org.photovision.vision.pipe.CVPipe;
+
 
 /**
 This sample program shows how to control a motor using a joystick.
@@ -24,7 +37,7 @@ public class Robot extends TimedRobot {
   private static final String kDefaultAuto = "Default";
   private static final String kCustomAuto = "My Auto";
   private String m_autoSelected;
-  private final SendableChooser<Command> m_chooser = new SendableChooser<>(); 
+  private final SendableChooser<String> m_chooser = new SendableChooser<>(); 
 
 
   @Override
@@ -33,6 +46,8 @@ public class Robot extends TimedRobot {
     m_chooser.setDefaultOption("Default Auto", kDefaultAuto);
     m_chooser.addOption("My Auto", kCustomAuto);
     SmartDashboard.putData("Auto choices", m_chooser);
+    SmartDashboard.putData(CommandScheduler.getInstance());
+
   }
 
 
@@ -68,4 +83,49 @@ public class Robot extends TimedRobot {
         autonomousCommand.cancel();
       }
     }
+  
+  double counter = 0.0;
+
+  @Override
+  public void teleopPeriodic() {
+    SmartDashboard.putNumber("Counter", counter++);
+
+  }
+
+  // set up USB camera capture
+  CameraServer.startAutomaticCapture();
+  CvSink cvSink = CameraServer.getVideo();
+
+  // set up AprilTag detector
+  AprilTagDetector detector = new AprilTagDetector();
+  AprilTagDetector.Config config = new AprilTagDetector.Config();
+  // set config parameters, e.g. config.blah = 5;
+  detector.setConfig(config);
+  detector.addFamily("tag16h5");
+
+  // Set up Pose Estimator
+  AprilTagPoseEstimator.Config poseEstConfig = new AprilTagPoseEstimator.Config(...);
+  AprilTagPoseEstimator estimator = new AprilTagPoseEstimator(poseEstConfig);
+
+  Mat mat = new Mat();
+  Mat graymat = new Mat();
+
+  while (!Thread.interrupted()) {
+  // grab image from camera
+    long time = cvSink.getFrame(mat);
+    if (time == 0) {
+      continue;  // error getting image
+    }
+
+    // convert image to grayscale
+    Imgproc.cvtColor(mat, graymat, Imgproc.COLOR_BGR2GRAY);
+  
+    // run detection
+    for (AprilTagDetection detection : detector.detect(graymat)) {
+    // filter by property
+
+      // run pose estimator
+      Transform3d pose = poseEstimator.estimate(detection);
+    }
+  }
 }
