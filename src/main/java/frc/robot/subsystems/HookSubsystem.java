62da4535864efcdc38dbc53aaa98c8c8f4a9d4f5
Tjbots15 <tanmayjena2027@eccrsd.us>
2024-01-31 15:41:06 -0500

    Fixed some errors with help

--- src/main/java/frc/robot/subsystems/HookSubsystem.java
+++ src/main/java/frc/robot/subsystems/HookSubsystem.java
@@ -10,44 +10,48 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 
 public class HookSubsystem extends SubsystemBase {
-    private CANSparkMax hookMotor = new CANSparkMax(Constants.MOTOR_CAN_ID, MotorType.kBrushless); // Create the Spark Max object in the program and include the CAN id and the type of motor used with the controller
-    private RelativeEncoder hookEncoder = hookMotor.getEncoder(); // Grab the attached encoder
+    private CANSparkMax hookMotor;  // Create the Spark Max object in the program and include the CAN id and the type of motor used with the controller
+    private RelativeEncoder hookEncoder; // Grab the attached encoder
     private final SparkPIDController hookController; 
-    hookController = hookMotor.getPIDController();
-    hookController.setP(0);
-    hookController.setI(0);
-    hookController.setD(0);
-    hookController.setFF(0);
+
+    public HookSubsystem() {
+        hookMotor = new CANSparkMax(Constants.MOTOR_CAN_ID, MotorType.kBrushless); // Create the Spark Max object in the program and include the CAN id and the type of motor used with the controller
+        hookEncoder = hookMotor.getEncoder(); // Grab the attached encoder
+        hookController = hookMotor.getPIDController();
+        hookController = hookMotor.getPIDController();
+        hookController.setP(0);
+        hookController.setI(0);
+        hookController.setD(0);
+        hookController.setFF(0);
+    }
+
+
     
+
+    
+    double i = hookEncoder.getPosition();
 
     //raises the hook motor
     public void raiseMotor() {
-        private CANSparkMax hookRaising = new CANSparkMax(Constants.MAXIMUMHOOK_HEIGHT, MotorType.kBrushless); 
+        //private CANSparkMax hookRaising = new CANSparkMax(Constants.MAXIMUMHOOK_HEIGHT, MotorType.kBrushless); 
         System.out.println("Raising Hook");
-        hookController.setReference(hookEncoder.getDegrees(), ControlType, kPosition);
-        hookController.setReference(0, ControlType.kPosition);
-        hookRaising.set(0.5);
-        double i = hookEncoder.getPosition();
-        while (i<5){
-            i = hookEncoder.getPosition();
-        }
-        hookRaising.stopMotor();
+        hookController.setReference(hookEncoder.getDegrees(), ControlType.kPosition);
     }
 
     //lowers the hook motor
-    public void downHook() {
+    public static void downHook() {
         private CANSparkMax hookDepth = new CANSparkMax(Constants.MAXIMUMHOOK_DEPTH, MotorType.kBrushless); 
         hookController.setReference(hook.getDegrees(), ControlType,kPosition);
         hookController.setReference(angle, ControlType.kPosition);
         System.out.println("Down Hook");
         hookDepth.set(-0.5);
         hookEncoder.setPosition(0);
-        double i = hookEncoder.getPosition();
         while (i>-5){
             i = hookEncoder.getPosition();
         }
         hookDepth.stopMotor();
     }
+}
     
     //Shuffleboard
     Shuffleboard.getTab("Hook Height")
@@ -109,6 +113,9 @@ public class HookSubsystem extends SubsystemBase {
         //SmartDashboard.putNumber("Back Left Drive Encoder", 0);
         //SmartDashboard.putNumber("Back Right Drive Encoder", 0);
         //SmartDashboard.putNumber("Swerve Angle", 0);
-    }
+        }
 
+    }
 }
+
+
